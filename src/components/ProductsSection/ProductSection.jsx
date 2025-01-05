1d945757ca8880770a299a735224e2ebd26e76d5
Your Name <s.r.j.s.n2017@gmail.com>
2025-01-05 09:51:13 +0530

    new update

--- src/components/ProductsSection/ProductSection.jsx
+++ src/components/ProductsSection/ProductSection.jsx
@@ -1,9 +1,13 @@
+import CategoryProducts from "../CategoryProducts/CategoryProducts"
+import HomeProducts from "../HomeProducts/HomeProducts"
 import TopProducts from "../TopProducts/TopProducts"
 
 const ProductSection = () => {
     return (
-        <div style={{ minHeight: '3056px', background: `linear-gradient( to bottom, rgb(236, 235, 238) 0%, rgba(255, 255, 255, 0) 100%)` }}>
+        <div style={{ background: `linear-gradient( to bottom, rgb(236, 235, 238) 0%, rgba(255, 255, 255, 0) 100%)` }}>
             <TopProducts />
+            <CategoryProducts />
+            <HomeProducts />
         </div>
     )
 }
